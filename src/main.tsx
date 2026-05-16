73e8c4a0a717aea6d845bb175ba8d16cb4991d1d
skkl22r-prog <skkl22r@gmail.com>
2026-05-16 06:02:56 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,8 @@
-import { createRoot } from "react-dom/client";
+import { createRoot } from '@supabase/supabase-js';
 import App from "./App.tsx";
 import "./index.css";
-
+const supabase = createClient(
+  'YOUR_SUPABASE_URL',
+  'YOUR_SUPABASE_ANON_KEY'
+);
 createRoot(document.getElementById("root")!).render(<App />);
