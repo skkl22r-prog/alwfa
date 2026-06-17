03551df56513f3d9e176c5cd30d5e7f50e58bb68
skkl22r-prog <skkl22r@gmail.com>
2026-06-17 13:43:09 +0300

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { MapPin, Heart, Baby, Camera, ChevronDown, Navigation } from "lucide-react";
-import invitationImg from "@/assets/invitation.jpg";
+import invitationImg from "@/assets/sarah1.jpg";
 import Envelope from "@/components/Envelope";
 import SprayParticles from "@/components/SprayParticles";
 import Reveal from "@/components/Reveal";
