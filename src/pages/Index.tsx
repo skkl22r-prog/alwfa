f99a690adf54aeb3b1ebcf0f9d5e950ee1bab483
skkl22r-prog <skkl22r@gmail.com>
2026-06-21 02:23:40 +0300

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -8,7 +8,7 @@ import Reveal from "@/components/Reveal";
 import Countdown from "@/components/Countdown";
 import Timeline from "@/components/Timeline";
 import MusicToggle from "@/components/MusicToggle";
-
+import Rsvp from "@/components/Rsvp";
 const Index = () => {
   const [opened, setOpened] = useState(false);
   const [atTop, setAtTop] = useState(true);
